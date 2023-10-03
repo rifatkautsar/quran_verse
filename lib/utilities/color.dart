a824a1d0ecb06ca2d8fdfd99b9f528874a8bfd96
rifatkautsar <kautsarrifat@gmail.com>
2023-10-03 10:06:30 +0700

    adding new color

--- lib/utilities/color.dart
+++ lib/utilities/color.dart
@@ -7,4 +7,6 @@ class ColorConstant {
   static const orangeColor = Color(0xFFF9B091);
   static const purpleColor2 = Color(0xFF3B1E77);
   static const purpleTextColor = Color(0xFFA19CC5);
+  static const itemReadColor1 = Color(0xFFDF98FA);
+  static const itemReadColor2 = Color(0xFF9055FF);
 }
